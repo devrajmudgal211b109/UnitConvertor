92cb721b29a89d1466a3394a8f38feba727ac598
Devraj Mudgal <devrajmudgal17@gmail.com>
2023-05-16 21:21:53 +0530

    Convertor Executed

--- app/src/main/java/com/example/unitconvertor/MainActivity.java
+++ app/src/main/java/com/example/unitconvertor/MainActivity.java
@@ -3,13 +3,17 @@ package com.example.unitconvertor;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button button;
     private TextView textView;
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +21,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         button=findViewById(R.id.button);
         textView=findViewById(R.id.textView);
+        editText=findViewById(R.id.editText);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Unit Convertor Working Successfully", Toast.LENGTH_SHORT).show();
+                String s=editText.getText().toString();
+                int Kg=Integer.parseInt(s);
+                double Pounds=Kg * 2.205;
+                textView.setText("The Corresponding Value of Pound is : "+Pounds);
+            }
+        });
     }
 }
