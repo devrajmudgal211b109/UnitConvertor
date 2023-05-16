6e94d553131892552cee6e9507356cb20435a050
Devraj Mudgal <devrajmudgal17@gmail.com>
2023-05-16 20:24:32 +0530

    type Modified

--- app/src/main/java/com/example/unitconvertor/MainActivity.java
+++ app/src/main/java/com/example/unitconvertor/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.unitconvertor;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button=findViewById(R.id.button);
+        textView=findViewById(R.id.textView);
     }
 }
